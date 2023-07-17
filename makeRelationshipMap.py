164ee297b7089458a60f208712a8e53bdca5e378
Alejandro Almanza <francisco.almanza@citi.com.mx>
2023-07-17 15:09:37 -0600

    :construction: Checkpoint: Trying to get epic,feature,dependencies

--- makeRelationshipMap.py
+++ makeRelationshipMap.py
@@ -27,6 +27,20 @@ def getServiceToken():
     return token
 
 
+# autopep8: off
+# Structure:
+    # {
+    #     Activities: => User Stories board - Epic
+    #     {
+            # Tasks:{ => Features
+                # TaskReleases: { => Releases (extra)
+                    # Subtasks: { => User Stories
+                    # }
+                # }
+            # }
+    #     }
+    # }
+# autopep8: on
 def getUserStoriesFromAPI():
     data_url = 'https://vectorcb.storiesonboard.com/api/q/storymapbyslugquery'
     data = {"QueryType": "StorymapBySlugQuery",
@@ -38,31 +52,66 @@ def getUserStoriesFromAPI():
         url=data_url, json=data, headers=headers)
     web_us_board = json.loads(story_map_response.text)
     epics = web_us_board["Activities"]
-    characteristics = []
-    releases = []  # User Stories are separated on releases inside of characteristics
+    features = []
+    releases = []
     web_USs = []
     for epic in epics:
-        characteristics.extend(epic["Tasks"])
-    for characteristic in characteristics:
-        releases.extend(characteristic["TaskReleases"])
+        features.extend(epic["Tasks"])
+    for f in features:
+        releases.extend(f["TaskReleases"])
     for release in releases:
         web_USs.extend(release["Subtasks"])
     # for us in web_USs:
     #     print(json.dumps(us["Title"], indent=" "))
-    return web_USs
+    return {'USs': web_USs, 'releases': releases, 'features': features, 'epics': epics}
 
 
-def writeDependenciesFile(uss):
+def searchReleaseById(releases, id):
+    for r in releases:
+        if r['ReleaseId'] == id:
+            # print(f"Release: {r['ReleaseId']}")
+            return r
+    raise ValueError("We didn't find that release id")
+
+
+def searchFeatureById(features, id):
+    for f in features:
+        if f['Id'] == id:
+            # print(f"Feature: {f['Id']} - {f['Title']}")
+            return f
+    raise ValueError("We didn't find that feature id")
+
+
+def writeDependenciesFile(uss, releases, features):
+    releases_involved = []
+    features_involved = []
     with open("./transitionFiles/UserStoriesRelationships.md", "w+") as relationshipfile:
         text = ''
+        correct_us = []
         for us in uss:
-            i = re.search("# DEPENDENCIAS", us["Description"], re.IGNORECASE)
-            if bool(i):
-                s = f"{us['Description'][i.start():]}"
-                s = s.replace('# DEPENDENCIAS', ' ')
-                s = s.replace('\n', '\n\t- ')
-                text = text + f"\n- {us['Title']} {s}"
+            dependencies_index = re.search(
+                "# DEPENDENCIAS", us["Description"], re.IGNORECASE)
+            if bool(dependencies_index):
+                # If there is not any release with that id, we add it
+                if not any(ri['ReleaseId'] == us['ReleaseId'] for ri in releases_involved):
+                    releases_involved.append(
+                        searchReleaseById(releases, us['ReleaseId']))
+                correct_us.append(us)
+        for ri in releases_involved:
+            # If there is not any release with that id, we add it
+            if not any(fi['Id'] == ri['ReleaseId'] for fi in features_involved):
+                features_involved.append(
+                    searchFeatureById(features, ri['TaskId']))
+            for us in correct_us:
+                dependencies_index = re.search(
+                    "# DEPENDENCIAS", us["Description"], re.IGNORECASE)
+                if bool(dependencies_index):
+                    s = f"{us['Description'][dependencies_index.start():]}"
+                    s = s.replace('# DEPENDENCIAS', ' ')
+                    s = s.replace('\n', '\n\t\t\t- ')
+                    text = text + f"\n\t\t- {us['Title']} {s}"
         print(text)
+        # print(len(features_involved))
         relationshipfile.write(text)
 
 # Syntaxis checked in this order Title, Description, Aceptance criteria, Dependencies
@@ -101,9 +150,9 @@ def checkSyntaxAndGetCleanList(USs: list):
 
 
 if __name__ == '__main__':
-    userStoriesGotten = getUserStoriesFromAPI()
+    userStoriesGotten, releases, features, epics = getUserStoriesFromAPI().values()
     USs, error_USs = checkSyntaxAndGetCleanList(userStoriesGotten).values()
-    writeDependenciesFile(USs)
+    writeDependenciesFile(USs, releases, features)
     # userStories = searchUSIdsFromWebPage(trimmedUserStoriesString)
     # # print(json.dumps(userStories, indent='  ', ensure_ascii=False))
 
